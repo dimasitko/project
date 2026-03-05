3c18163a58a1bfe4151479b44a4bf7ec76193350
Дмитро Сітько <dimasitko65@gmail.com>
2026-03-05 22:52:32 +0200

    update main

--- lr2/src/index.js
+++ lr2/src/index.js
@@ -6,6 +6,7 @@ const app = express();
 
 const PORT = process.env.PORT ?? 3000;
 app.use(express.json());
+app.use(express.static('public'));
 
 app.get("/health", (req, res) => res.status(200).json({ ok: true }));
 
