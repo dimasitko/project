456e8af9509a5e14fb9830e91713f0fb569f3ca3
Дмитро Сітько <dimasitko65@gmail.com>
2026-03-24 10:57:36 +0200

    add CHECK to passes,users,logs tables

--- src/db/migrations/001_users.sql
+++ src/db/migrations/001_users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     email VARCHAR UNIQUE NOT NULL,
-    name VARCHAR NOT NULL,
-    role VARCHAR NOT NULL,
+    name VARCHAR NOT NULL CHECK(length(name) >= 2),,
+    role VARCHAR NOT NULL CHECK(role IN ('Вчитель', 'Студент', 'Адміністратор', 'Техперсонал')),
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
--- src/db/migrations/002_passes.sql
+++ src/db/migrations/002_passes.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS passes (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
     admin_id INTEGER NOT NULL,
-    status VARCHAR NOT NULL,
-    date VARCHAR NOT NULL,
+    status VARCHAR NOT NULL CHECK(status IN ('Вчитель', 'Студент', 'Техперсонал')),
+    date VARCHAR NOT NULL CHECK(length(date) >= 10)
     comment VARCHAR,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id),
